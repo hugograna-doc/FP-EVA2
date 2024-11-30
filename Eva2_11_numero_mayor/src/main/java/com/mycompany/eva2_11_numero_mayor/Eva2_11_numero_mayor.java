a1a55e475f14e35f812819ba47b031e9eded04e8
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-29 19:04:11 -0600

    Add files via upload

---
+++ Eva2_11_numero_mayor/src/main/java/com/mycompany/eva2_11_numero_mayor/Eva2_11_numero_mayor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_11_numero_mayor;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class Eva2_11_numero_mayor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
