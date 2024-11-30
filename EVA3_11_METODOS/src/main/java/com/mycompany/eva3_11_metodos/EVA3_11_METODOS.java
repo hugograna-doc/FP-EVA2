0bb8e3efc65b9394d18657658579b4e427e96011
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-30 00:00:02 -0600

    Add files via upload

---
+++ EVA3_11_METODOS/src/main/java/com/mycompany/eva3_11_metodos/EVA3_11_METODOS.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_11_metodos;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class EVA3_11_METODOS {
+
+    public static void main(String[] args) {
+       //INVOCAR EL METODO, LLAMADA A MÉTODO --->USARLO
+       imprimirMensaje("Hola Mundo!!");
+        
+    }
+   
+    //PUBLIC Y STATIC SON MODIFICACORES  SI ES PUBLICO O PRIBADO
+        //VOID QUE REGRESA ENTERO
+        //Imprimir mensaje Parametros
+   //() PARAMETROS Y SON OPCIONALES
+    public static void imprimirMensaje(String mensaje){
+        //cuerpo de metodo
+        System.out.println("Tu mensaje:" + mensaje);
+    }
+
+
+
+}
