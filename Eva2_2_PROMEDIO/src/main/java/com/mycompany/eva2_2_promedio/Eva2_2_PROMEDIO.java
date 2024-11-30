f31763751e22b1250db67fad20d249067d6da9ba
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-29 18:36:37 -0600

    Add files via upload

---
+++ Eva2_2_PROMEDIO/src/main/java/com/mycompany/eva2_2_promedio/Eva2_2_PROMEDIO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_2_promedio;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class Eva2_2_PROMEDIO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
