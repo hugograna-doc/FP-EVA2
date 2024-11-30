362bccb75a71d3a14992a69048823ec3d8ffc831
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-29 23:56:42 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/main/java/com/mycompany/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class EVA3_1_METODOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
