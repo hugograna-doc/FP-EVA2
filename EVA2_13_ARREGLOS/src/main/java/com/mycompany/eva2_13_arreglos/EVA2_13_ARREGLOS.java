600e0d7b1bee549b5f312abbbcd760337e0fdec8
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-29 21:08:32 -0600

    Add files via upload

---
+++ EVA2_13_ARREGLOS/src/main/java/com/mycompany/eva2_13_arreglos/EVA2_13_ARREGLOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_13_arreglos;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class EVA2_13_ARREGLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
