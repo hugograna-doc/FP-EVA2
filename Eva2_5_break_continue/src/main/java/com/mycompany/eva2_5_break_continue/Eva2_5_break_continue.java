61f3c3ba1bb2ecc5ac4124e017f47b3c268f3e19
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-29 18:43:42 -0600

    Add files via upload

---
+++ Eva2_5_break_continue/src/main/java/com/mycompany/eva2_5_break_continue/Eva2_5_break_continue.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_5_break_continue;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class Eva2_5_break_continue {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
