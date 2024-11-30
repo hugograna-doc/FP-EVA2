2ba9597c565d986df4255e786d287472884f7ccb
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-29 19:05:50 -0600

    Add files via upload

---
+++ Eva2_12_tipos_de_datos/src/main/java/com/mycompany/eva2_12_tipos_de_datos/Eva2_12_tipos_de_datos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_12_tipos_de_datos;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class Eva2_12_tipos_de_datos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
