7a5287e9438a8cedd05305e18bbc110dad9f1747
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-29 19:00:31 -0600

    Add files via upload

---
+++ Eva2_7_while/src/main/java/com/mycompany/eva2_7_while/Eva2_7_while.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_7_while;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class Eva2_7_while {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
