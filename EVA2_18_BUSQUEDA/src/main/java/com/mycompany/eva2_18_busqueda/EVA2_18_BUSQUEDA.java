78b40cb2d50d9b625f293f093c1ecfa357bc32c5
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-29 21:25:57 -0600

    Add files via upload

---
+++ EVA2_18_BUSQUEDA/src/main/java/com/mycompany/eva2_18_busqueda/EVA2_18_BUSQUEDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_18_busqueda;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class EVA2_18_BUSQUEDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
